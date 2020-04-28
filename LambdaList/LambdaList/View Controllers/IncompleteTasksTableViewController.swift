eecfa772d11dffde35aea76207889943c62f054f
Shawn <shawn.james@me.com>
2020-04-27 20:31:23 -0400

    fix IncompleteTasksTableViewController indentation

--- LambdaList/LambdaList/View Controllers/IncompleteTasksTableViewController.swift
+++ LambdaList/LambdaList/View Controllers/IncompleteTasksTableViewController.swift
@@ -9,36 +9,36 @@
 import UIKit
 
 class IncompleteTasksTableViewController: UITableViewController {
-
+    
     // MARK: - Properties
-        
-        // MARK: - Lifecycle
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-        }
-
-        // MARK: - Table view data source
-
-        override func numberOfSections(in tableView: UITableView) -> Int {
-            // TODO: ? - number of sections
-            return 0
-        }
-
-        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            // TODO: ? - number of rows
-            return 0
-        }
-/*
-        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: <#add identifier here#>, for: indexPath)
-            // TODO: ? - add identifier above ^
-            // TODO: ? - dependency injection
-            // Configure the cell...
-
-            return cell
-        }
-*/
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // TODO: ? - number of sections
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO: ? - number of rows
+        return 0
+    }
+    /*
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: <#add identifier here#>, for: indexPath)
+     // TODO: ? - add identifier above ^
+     // TODO: ? - dependency injection
+     // Configure the cell...
+     
+     return cell
+     }
+     */
     //    // Override to support editing the table view.
     //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
     //        if editingStyle == .delete {
@@ -48,29 +48,29 @@ class IncompleteTasksTableViewController: UITableViewController {
     //            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     //        }
     //    }
-
-        /*
-        // Override to support rearranging the table view.
-        override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-        }
-        */
-
-        /*
-        // Override to support conditional rearranging of the table view.
-        override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-            // Return false if you do not want the item to be re-orderable.
-            return true
-        }
-        */
-
-        // MARK: - Navigation
-
-        
-        // In a storyboard-based application, you will often want to do a little preparation before navigation
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            // TODO: ? - Get the new view controller using segue.destination.
-            // TODO: ? - Pass the selected object to the new view controller.
-        }
-
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    // MARK: - Navigation
+    
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // TODO: ? - Get the new view controller using segue.destination.
+        // TODO: ? - Pass the selected object to the new view controller.
+    }
+    
 }
