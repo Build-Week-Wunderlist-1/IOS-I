ae3898910f331cf4c15b569195b02c08ddbad777
Shawn <shawn.james@me.com>
2020-04-28 16:59:59 -0400

    Add managed object convenience init for Task pt. 2

---
+++ LambdaList/LambdaList/Model/Task+Convenience.swift
@@ -0,0 +1,31 @@
+//
+//  Task+Convenience.swift
+//  LambdaList
+//
+//  Created by Shawn James on 4/27/20.
+//  Copyright © 2020 iOS BW. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Task {
+    
+    convenience init(taskName: String,
+                     taskID: Int64,
+                     taskDescription: String,
+                     sort: Int64 = 1,
+                     createdDate: Date = Date(),
+                     completed: Bool = false,
+                     context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        self.taskName = taskName
+        self.taskID = taskID
+        self.taskDescription = taskDescription
+        self.sort = sort
+        self.createdDate = createdDate
+        self.completed = completed
+    }
+    
+}
