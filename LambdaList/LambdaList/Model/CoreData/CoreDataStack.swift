def43f49c2325d2a3cc3fbfb0694cd2113b2b095
Shawn <shawn.james@me.com>
2020-04-27 20:20:40 -0400

    Implement CoreDataStack (coredatamanager)

--- LambdaList/LambdaList/Model/CoreData/CoreDataStack.swift
+++ LambdaList/LambdaList/Model/CoreData/CoreDataStack.swift
@@ -10,7 +10,25 @@ import Foundation
 import CoreData
 
 class CoreDataStack {
- 
-    // TODO: Mark - implement coredata stack
+        
+    // create singleton
+    static let shared = CoreDataStack()
+    
+    // create container
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Task")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        
+        return container
+    }()
+    
+    // create context -> everything done in the app uses the context
+    var mainContext: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
     
 }
