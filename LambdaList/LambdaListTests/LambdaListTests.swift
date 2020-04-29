ca1bb7955ae76f8a547d118fdb439dd64c4fe077
Mark Gerrior <github@gerrior.com>
2020-04-29 13:37:21 -0700

    Added Unit Test and UI Tests to project.

---
+++ LambdaList/LambdaListTests/LambdaListTests.swift
@@ -0,0 +1,33 @@
+//
+//  LambdaListTests.swift
+//  LambdaListTests
+//
+//  Created by Mark Gerrior on 4/29/20.
+//  Copyright © 2020 iOS BW. All rights reserved.
+//
+
+import XCTest
+
+class LambdaListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
