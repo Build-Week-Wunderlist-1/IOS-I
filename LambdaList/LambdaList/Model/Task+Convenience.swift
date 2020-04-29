e8ab8fd8b66b3b98a0d618f9fea6d863886be6f8
Mark Gerrior <github@gerrior.com>
2020-04-29 12:24:39 -0700

    Flushed out TaskRepresentation for backend usage. Added modified date to original convenience init. Added convenience init for backend.

--- LambdaList/LambdaList/Model/Task+Convenience.swift
+++ LambdaList/LambdaList/Model/Task+Convenience.swift
@@ -12,16 +12,17 @@ import CoreData
 /// Because we choose Codegen: "class definition" in Tasks.xcdaatamodeld, Task gets generated behind the scenes
 extension Task {
 
-//    var taskRepresentation: TaskRepresentation? {
-//        guard let taskName = taskName else { return nil }
-//
-//        return TaskRepresentation(taskName: taskName,
-//                                  taskID: taskID,
-//                                  taskDescription: taskDescription,
-//                                  sort: sort,
-//                                  createdDate: createdDate,
-//                                  completed: completed)
-//    }
+    var taskRepresentation: TaskRepresentation? {
+        guard let taskName = taskName else { return nil }
+
+        return TaskRepresentation(taskName: taskName,
+                                  taskID: Int(taskID),
+                                  taskDescription: taskDescription ?? "",
+                                  sort: Int(sort),
+                                  createdDate: createdDate ?? Date(),
+                                  modifiedDate: modifiedDate ?? Date(),
+                                  completed: completed)
+    }
 
     /// This is the original init before we added REST API
     @discardableResult convenience init(taskName: String,
@@ -29,6 +30,7 @@ extension Task {
                      taskID: Int = 1,
                      sort: Int = 1,
                      createdDate: Date = Date(),
+                     modifiedDate: Date = Date(),
                      completed: Bool = false,
                      context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
 
@@ -40,22 +42,22 @@ extension Task {
         self.taskDescription = taskDescription
         self.sort = Int64(sort)
         self.createdDate = createdDate
+        self.modifiedDate = modifiedDate
         self.completed = completed
     }
 
     /// Convenience Initializer
-    /// Items coming in from Firebase for CoreData
-//    @discardableResult convenience init?(entryRepresentation: EntryRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
-//        /// mood comes in as a string from Firebase. We need to convert to Mood enum
-//        guard let mood = Mood(rawValue: entryRepresentation.mood) else {
-//                return nil
-//        }
-//
-//        self.init(identifier: entryRepresentation.identifier,
-//                  title: entryRepresentation.title,
-//                  bodyText: entryRepresentation.bodyText,
-//                  timestamp: entryRepresentation.timestamp,
-//                  mood: mood,
-//                  context: context)
-//    }
+    /// Items coming in from backend for CoreData
+    @discardableResult convenience init?(taskRepresentation: TaskRepresentation,
+                                         context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+
+        self.init(taskName: taskRepresentation.taskName,
+                  taskDescription: taskRepresentation.taskDescription,
+                  taskID: taskRepresentation.taskID,
+                  sort: taskRepresentation.sort,
+                  createdDate: taskRepresentation.createdDate,
+                  modifiedDate: taskRepresentation.modifiedDate,
+                  completed: taskRepresentation.completed,
+                  context: context)
+    }
 }
