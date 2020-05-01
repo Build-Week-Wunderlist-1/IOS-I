f2b7bf7ea97c816f956966f46d8f5007e7bbfa6c
Mark Gerrior <github@gerrior.com>
2020-05-01 02:51:44 -0700

    snapshot

--- LambdaList/LambdaListUITests/LambdaListUITests.swift
+++ LambdaList/LambdaListUITests/LambdaListUITests.swift
@@ -12,6 +12,8 @@ class LambdaListUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let app = XCUIApplication()
+        app.launch()
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
@@ -23,13 +25,35 @@ class LambdaListUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testLogIn() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
-        app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(!app.navigationBars["Lambda List"].buttons["Logout"].exists, "⚠️ Not on Login Screen")
+
+        app.segmentedControls.buttons["Log In"].tap()
+        app.textFields["Username:"].tap()
+        app.textFields["Username:"].typeText("gerrior04")
+
+        let passwordSecureTextField = app.secureTextFields["Password"]
+
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("123456")
+
+        app.buttons["Log In"].tap()
+
+        XCTAssert(app.navigationBars["Lambda List"].staticTexts["Lambda List"].exists, "⚠️ Was not able to login")
+
+//        app.navigationBars["Lambda List"].buttons["Logout"].tap()
+//        app/*@START_MENU_TOKEN@*/.buttons["Sign Up"]/*[[".segmentedControls.buttons[\"Sign Up\"]",".buttons[\"Sign Up\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+//        app.segmentedControls.buttons["Sign Up"].tap()
+//        app.buttons["square"].tap()
+
+
+    }
+
+    func testLogOut() throws {
+        XCTAssert(!app.navigationBars["Lambda List"].buttons["Logout"].exists, "⚠️ Not on Login Screen")
     }
 
     func testLaunchPerformance() throws {
