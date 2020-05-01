8b59a8c22fec1dfbca4b825d818b6dd599d0e48a
Mark Gerrior <github@gerrior.com>
2020-04-30 17:39:53 -0700

    Print location of SQL Lite database on launch

--- LambdaList/LambdaList/AppDelegate.swift
+++ LambdaList/LambdaList/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // Location of *.sqlite database.
+        print(NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true).last! as String)
+        
         return true
     }
 
